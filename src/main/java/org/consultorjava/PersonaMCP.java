6d606269377922f8bc3b0134fba7cdee46179e8d
jlbugarin <jlbugarin@gmail.com>
2025-11-23 00:35:43 -0500

    actualizacion de carga de datos en tabla
    update

--- src/main/java/org/consultorjava/PersonaMCP.java
+++ src/main/java/org/consultorjava/PersonaMCP.java
@@ -1,17 +1,27 @@
 package org.consultorjava;
 
+import java.util.List;
+
 import io.quarkiverse.mcp.server.Tool;
 import io.quarkiverse.mcp.server.ToolArg;
 
 public class PersonaMCP {
 
 
-    @Tool(description = "listar a los propietarios registrados")
-        String getPropietarios(@ToolArg(description = "listar propietarios") String inputChat) {
+   /*  @Tool(description = "listar a los propietarios registrados")
+        String getPersonas(@ToolArg(description = "listar propietarios") String inputChat) {
                 
             //logica para traer a los propietarios
+            Persona.listAll();
 
-            return "propietariossss";
-        }
+            return "personas registradas";
+        }*/
     
+     @Tool(description = "listar a los propietarios registrados")
+        public List<Persona> getAll(@ToolArg(description = "listar personas") String inputChat) {
+        return Persona.listAll();
+    }
+
+
+       
 }
